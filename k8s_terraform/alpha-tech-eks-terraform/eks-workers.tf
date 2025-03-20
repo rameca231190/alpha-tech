7932259359305a84d460b4255f7e8d65bc565c53
rameca231190 <romapereverziev@gmail.com>
2025-03-20 14:40:06 -0500

    workers

--- k8s_terraform/alpha-tech-eks-terraform/eks-workers.tf
+++ k8s_terraform/alpha-tech-eks-terraform/eks-workers.tf
@@ -1,25 +1,32 @@
-# EKS currently documents this required userdata for EKS worker nodes to
-# properly configure Kubernetes applications on the EC2 instance.
-# We utilize a Terraform local here to simplify Base64 encoding this
-# information into the AutoScaling Launch Configuration.
-# More information: https://docs.aws.amazon.com/eks/latest/userguide/launch-workers.html
 locals {
-  node-userdata = <<USERDATA
-#!/bin/bash
-set -o xtrace
-/etc/eks/bootstrap.sh --apiserver-endpoint '${aws_eks_cluster.cluster_eks.endpoint}' --b64-cluster-ca '${aws_eks_cluster.cluster_eks.certificate_authority[0].data}' '${var.cluster-name}'
-USERDATA
-
+  node_userdata = <<-USERDATA
+    #!/bin/bash
+    set -o xtrace
+    /etc/eks/bootstrap.sh ${var.cluster-name} --apiserver-endpoint '${aws_eks_cluster.cluster_eks.endpoint}' --b64-cluster-ca '${aws_eks_cluster.cluster_eks.certificate_authority[0]["data"]}'
+  USERDATA
 }
 
-resource "aws_launch_configuration" "cluster_eks" {
-  associate_public_ip_address = false
-  iam_instance_profile = aws_iam_instance_profile.node.name
-  image_id = var.image_id
+resource "aws_launch_template" "cluster_eks" {
+  name_prefix   = "terraform-eks-${var.env}"
+  image_id      = var.image_id
   instance_type = "t3.medium"
-  name_prefix = "terraform-eks-${var.env}"
-  security_groups = [aws_security_group.node.id]
-  user_data_base64 = base64encode(local.node-userdata)
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.node.name
+  }
+
+  network_interfaces {
+    security_groups = [aws_security_group.node.id]
+  }
+
+  user_data = base64encode(local.node_userdata)
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "terraform-eks-${var.env}"
+    }
+  }
 
   lifecycle {
     create_before_destroy = true
@@ -27,31 +34,27 @@ resource "aws_launch_configuration" "cluster_eks" {
 }
 
 resource "aws_autoscaling_group" "cluster_eks" {
-  desired_capacity = 3
-  launch_configuration = aws_launch_configuration.cluster_eks.id
-  max_size = 4
-  min_size = 2
-  name = "terraform-eks-${var.env}"
-  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-  # force an interpolation expression to be interpreted as a list by wrapping it
-  # in an extra set of list brackets. That form was supported for compatibilty in
-  # v0.11, but is no longer supported in Terraform v0.12.
-  #
-  # If the expression in the following list itself returns a list, remove the
-  # brackets to avoid interpretation as a list of lists. If the expression
-  # returns a single list item then leave it as-is and remove this TODO comment.
+  desired_capacity     = 3
+  max_size            = 4
+  min_size            = 2
   vpc_zone_identifier = var.private_subnets
 
+  launch_template {
+    id      = aws_launch_template.cluster_eks.id
+    version = "$Latest"
+  }
+
   tag {
-    key = "Name"
-    value = "terraform-eks-${var.env}"
+    key                 = "Name"
+    value               = "terraform-eks-${var.env}"
     propagate_at_launch = true
   }
 
   tag {
-    key = "kubernetes.io/cluster/${var.cluster-name}"
-    value = "owned"
+    key                 = "kubernetes.io/cluster/${var.cluster-name}"
+    value               = "owned"
     propagate_at_launch = true
   }
+
+  depends_on = [aws_launch_template.cluster_eks]
 }
-
